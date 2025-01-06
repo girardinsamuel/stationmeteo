6d8541366253fa7e9baeeebc030345bd3e280a0c
Samuel Girardin <samuelgirardin@pm.me>
2025-01-06 15:32:23 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,109 @@
+from datetime import datetime
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from typing import List
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+from sqlalchemy import ForeignKey
+import random
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+db = SQLAlchemy(model_class=Base)
+
+
+# class Sensor(Base):
+#     __tablename__ = "sensor_table"
+
+#     id: Mapped[int] = mapped_column(primary_key=True)
+#     name: Mapped[str] = mapped_column(unique=True)
+#     logs: Mapped[List["DataLog"]] = relationship("DataLog", backref="sensor")
+#     # comments = db.relationship('Comment', backref='post')
+
+
+# class DataLog(Base):
+#     __tablename__ = "logs_table"
+
+#     id: Mapped[int] = mapped_column(primary_key=True)
+#     temperature: Mapped[float] = mapped_column()
+#     humidity: Mapped[float] = mapped_column()
+#     sensor_id = db.Column(db.Integer, ForeignKey("sensor.id"))
+
+
+class Sensor(db.Model):
+    __tablename__ = "sensors"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    logs = db.relationship("Log", back_populates="sensor")
+
+
+class Log(db.Model):
+    __tablename__ = "logs"
+    id = db.Column(db.Integer, primary_key=True)
+    timestamp = db.Column(db.DateTime)
+    temperature = db.Column(db.Float)
+    pressure = db.Column(db.Float)
+    humidity = db.Column(db.Float)
+    rain = db.Column(db.Float)
+    light = db.Column(db.Float)
+    battery = db.Column(db.Integer)
+    sensor_id = db.Column(db.Integer, db.ForeignKey("sensors.id"))
+    sensor = db.relationship("Sensor", back_populates="logs")
+
+
+app = Flask(__name__)
+# configure the SQLite database, relative to the app instance folder
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///weather_db.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+# initialize the app with the extension
+db.init_app(app)
+
+
+@app.route("/")
+def home():
+    """Show last measurement of all sensors."""
+    # sensors = [
+    #     {"name": "Maison", "id": 1, "data": {"temperature": 20, "humidity": 50}},
+    #     {"name": "Jardin", "id": 2, "data": {"temperature": 16, "humidity": 80}},
+    # ]
+    sensors = Sensor.query.all()
+    for sensor in sensors:
+        sensor.last_log = sensor.logs[-1] if sensor.logs else None
+
+    return render_template("home.html", sensors=sensors)
+
+
+@app.route("/modules/<sensor_id>")
+def sensor_detail(sensor_id):
+    # sensor = {
+    #     "name": "Maison",
+    #     "id": sensor_id,
+    #     "data": {"temperature": 20, "humidity": 50},
+    # }
+    sensor = Sensor.query.get_or_404(sensor_id)
+
+    return render_template("sensor.html", sensor=sensor)
+
+
+@app.route("/modules/<sensor_id>/log")
+def sensor_log(sensor_id):
+    """View for testing only."""
+    # sensor = {
+    #     "name": "Maison",
+    #     "id": sensor_id,
+    #     "data": {"temperature": 20, "humidity": 50},
+    # }
+
+    log = Log(
+        sensor_id=sensor_id,
+        timestamp=datetime.now(),
+        temperature=random.randint(10, 25),
+        humidity=random.randint(40, 65),
+    )
+    db.session.add(log)
+    db.session.commit()
+
+    return f"Logged to {sensor_id}"
---
+++ init_db.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+from app import db, Sensor, Log, app
+
+with app.app_context():
+    db.drop_all()
+    db.create_all()
+    sensor1 = Sensor(name="Maison", id=1, simple=True)
+    sensor2 = Sensor(name="Jardin", id=2, simple=False)
+    sensor3 = Sensor(name="Gîte", id=3, simple=True)
+
+    db.session.add_all([sensor1, sensor2, sensor3])
+    db.session.commit()
